e44b41f2205898ca7120bf509183a24256e74afe
Murillo Fagundes da Silva <11171504063@alunos.umc.br>
2020-07-27 21:57:16 -0300

    Descrição mudança

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área"}>
+        videoDescription={"Maiores sucessos de Travis Scott"}>
       </BannerMain>
 
       <Carousel 
